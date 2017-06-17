c29ce865d92ef5fca6b09d38e830995fb2ef1080
嶋田純 <shimadajun@shimadajun-no-MacBook-Air.local>
2017-06-17 17:05:19 +0900

    my first commit

---
+++ 09/session.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+
+
+$sid=session_id();
+
+echo session_id();
+
+
+
+
+
+
+
+
+?>
---
+++ 09/test.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+
+
+//$sid=session_id();
+
+//echo session_id();
+
+
+echo $_SESSION["name"];
+echo $_SESSION["flg"];
+
+ 
+
+
+
+
+?>
